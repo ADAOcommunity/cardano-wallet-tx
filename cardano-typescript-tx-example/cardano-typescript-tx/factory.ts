e94251c4b0bc1327c997b6eb0227f3962cae305f
zachyking <dominik.zachar@gmail.com>
2022-04-03 21:32:14 +0200

    initial commit

---
+++ cardano-typescript-tx-example/cardano-typescript-tx/factory.ts
@@ -0,0 +1,15 @@
+import { CardanoWallet } from './index';
+
+export class Factory {
+    private _instance?: CardanoWallet;
+
+    public get instance() {
+        return this._instance;
+    }
+
+    public async load() {
+        if (!this.instance)
+            this._instance = new CardanoWallet(await import('./custom_modules/@emurgo/cardano-serialization-lib-browser'));
+        return this.instance;
+    }
+}
---
+++ plutus/alwaysSucceedsValidator.ts
@@ -0,0 +1,8 @@
+const AlwaysSucceedsValidator = {
+   validator: '4e4d01000033222220051200120011',
+   address: 'addr1w9nlxv2xv9a9ucvnvzqakwepzl9ltx7jzgm53av2e9ncv4slcd85z',
+   testnetAddress: 'addr_test1wpnlxv2xv9a9ucvnvzqakwepzl9ltx7jzgm53av2e9ncv4sysemm8',
+   exBudget: {exBudgetCPU: 297830, exBudgetMemory: 1100}
+}
+
+export default AlwaysSucceedsValidator

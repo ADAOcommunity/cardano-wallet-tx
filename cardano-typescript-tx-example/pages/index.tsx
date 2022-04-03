4b1e42025bbc108bcc0aae5bd95709fd5b49474a
zachyking <dominik.zachar@gmail.com>
2022-04-03 21:35:39 +0200

    remove lib files to main dir

--- cardano-typescript-tx-example/pages/index.tsx
+++ cardano-typescript-tx-example/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import useCardanoWallet  from '../cardano-typescript-tx/useCardanoWallet'
+import useCardanoWallet  from '../useCardanoWallet'
 const Home: NextPage = () => {
   const wallet = useCardanoWallet()
   console.log('wallet')

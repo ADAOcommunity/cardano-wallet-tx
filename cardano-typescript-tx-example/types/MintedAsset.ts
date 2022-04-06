5baa0ce9d6f9c00914e5d4e68eb9047f63cfa732
dzCodes <dominik.zachar@gmail.com>
2022-04-06 10:47:47 +0200

    remove obsolette code

--- cardano-typescript-tx-example/types/MintedAsset.ts
+++
@@ -1,7 +0,0 @@
-export default interface MintedAsset {
-    assetName: string;
-    quantity: string;
-    policyId: string;
-    policyScript: string;
-    address?: string;
-};
--- cardano-typescript-tx-example/wallet-config.ts
+++
@@ -1,8 +0,0 @@
-export const walletConfig = {
-    'nami': typeof window === 'undefined' ? {} : (window as any).cardano.nami,
-    'eternl': typeof window === 'undefined' ? {} : (window as any).cardano.eternl, //  ==   'ccvault': typeof window === 'undefined' ? {} : (window as any).cardano.ccvault
-    'flint': typeof window === 'undefined' ? {} : (window as any).cardano.flint,
-    'yoroi': typeof window === 'undefined' ? {} : (window as any).cardano.yoroi,
-    'cardwallet': typeof window === 'undefined' ? {} : (window as any).cardano.cardwallet,
-    'gerowallet': typeof window === 'undefined' ? {} : (window as any).cardano.gerowallet
-}

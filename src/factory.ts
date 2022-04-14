c89896ff5101beae56153df8ef9c0a044a78f138
Justin Bender <breezy@Justins-MacBook-Pro.local>
2022-04-14 04:34:34 -0400

    adding tsdocs examples

--- src/factory.ts
+++ src/factory.ts
@@ -1,13 +1,56 @@
 import { CardanoWallet } from './cardanoWallet'
+// Copyright (c) ADAOcommunity. All rights reserved. Licensed under the Apache License 2.0 license.
 
+/**
+ * 🚧 Allows creating transactions based on configuration, including minting and plutus scripts. Checkout development: {@link https://github.com/ADAOcommunity/cardano-wallet-tx | github}
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * Factory for CardanoWallet Instance
+ *
+ * @remarks
+ * Imports the @emurgo/cardano-serialization-lib-browser
+ * ```ts
+ * new CardanoWallet(await import('@emurgo/cardano-serialization-lib-browser'))
+ * ```
+ * @returns CardanoWallet Instance
+ *
+ * @public
+ */
 export class Factory {
   private _instance?: CardanoWallet
 
+    /**
+   * Get Factory Instance.
+   *
+   * @example
+   *
+   * ```ts
+   * ```
+   * @readonly
+   */
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   public get instance() {
     return this._instance
   }
 
+    /**
+   * Cardano Serialization Lib Factory Loader.
+   *
+   * @remarks
+   * This method attaches the cardano-serialization-lib-browser.
+   *
+   * @example
+   *
+   * ```ts
+   * ```
+   *
+   * @eventProperty
+   *
+   * @public
+   */
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   public async load() {
     if (!this.instance)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+// Copyright (c) ADAOcommunity. All rights reserved. Licensed under the Apache License 2.0 license.
+
+/**
+ * 🚧 Allows creating transactions based on configuration, including minting and plutus scripts. For details, see {@link https://github.com/ADAOcommunity/cardano-wallet-tx | github}
+ *
+ * @remarks
+ *Allows creating transactions based on configuration, including minting and plutus scripts
+
+It uses npm, TypeScript compiler, Jest, webpack, ESLint, Prettier, husky, pinst, commitlint. The production files include CommonJS, ES Modules, UMD version and TypeScript declaration files.
+
+For Cardano related operations it uses custom version of cardano serialization library and Cardano Koios or Graphql instance to for chain data.
+ *
+ * @packageDocumentation
+ */
+
 import CardanoWallet from './cardanoWallet'
 import { Value, ProtocolParameters, UTxO } from './queryApi'
 import {
